73d90d8cfe52656edf031d80127d9010bc613490
Patrick <patricktsaikl@gmail.com>
2023-01-27 13:14:32 +0000

    footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Main from './Main';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/app/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="copyright">&copy; Lorem ipsum dolor sit amet consectetur</p>
+    </div>
+  );
+};
+
+export default Footer;
