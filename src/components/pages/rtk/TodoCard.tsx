5a2e07c3b5ff1bd1a05e7e7e5e3017439ecc45b6
Patrick <patricktsaikl@gmail.com>
2023-02-08 11:05:09 +0000

    add delete todo button

--- src/components/pages/rtk/TodoCard.tsx
+++ src/components/pages/rtk/TodoCard.tsx
@@ -3,12 +3,18 @@ import Card from 'react-bootstrap/Card';
 import { TodoState } from '../../../redux/features/todos/todosSlice';
 import './todo-card.scss';
 import Button from 'react-bootstrap/Button';
+import { useAppDispatch } from '../../../redux/hooks';
+import { deleteOneTodo } from '../../../redux/features/todos/todosSlice';
 
 type TodoCardType = { todoState: TodoState };
 
 function TodoCard({ todoState }: TodoCardType) {
+  const dispatch = useAppDispatch();
   const { title, content, id, createAt } = todoState;
   const parsedTime = new Date(createAt);
+  function handleDeleteTodo(): void {
+    dispatch(deleteOneTodo(id));
+  }
   function renderContent(content: string) {
     const jokeUrlInd = content.indexOf(
       'joke link: https://icanhazdadjoke.com/'
@@ -17,6 +23,7 @@ function TodoCard({ todoState }: TodoCardType) {
       return <p>{content}</p>;
     }
     const jokeUrl = content.substring(jokeUrlInd + 11);
+
     return (
       <p>
         {content.substring(0, jokeUrlInd)}
@@ -28,10 +35,14 @@ function TodoCard({ todoState }: TodoCardType) {
       </p>
     );
   }
+
   return (
     <Card className="todo-card">
       <Card.Header>
         <h5> {title}</h5>
+        <Button aria-label="Delete" onClick={handleDeleteTodo} variant="danger">
+          <div>&#120;</div>
+        </Button>
       </Card.Header>
       <Card.Body>
         <blockquote className="blockquote mb-0">
