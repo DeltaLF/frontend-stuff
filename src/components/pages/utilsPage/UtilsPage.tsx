45f671d8beedc862a5989618841b684bd3ae8cdb
Patrick <patricktsaikl@gmail.com>
2023-03-19 17:27:01 +0800

    useForm hook

--- src/components/pages/utilsPage/UtilsPage.tsx
+++ src/components/pages/utilsPage/UtilsPage.tsx
@@ -1,42 +1,15 @@
 import React from 'react';
-import LoadingWrapper from '../../common/loading/LoadingWrapper';
+import ShowArticle from '../../common/loading/ShowArticle';
+import FormComponent from '../../../hooks/useForm/FormComponent';
 
 const UtilsPage = () => {
   return (
     <div>
       Utils page
       <ShowArticle />
+      <FormComponent />
     </div>
   );
 };
 
 export default UtilsPage;
-
-function ShowArticle() {
-  function reFetchArticle() {
-    // axois.get('/articles')
-    // mimic get from server
-    return new Promise<string>((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.floor(Math.random() * 2) === 0) {
-          resolve('article one');
-        } else {
-          reject('fail');
-        }
-      }, 1000);
-    });
-  }
-  function renderArticle(data: string | number) {
-    return <div>{data}</div>;
-  }
-  return (
-    <>
-      <span>article</span>
-      <button onClick={reFetchArticle}>Refetch article</button>
-      <LoadingWrapper<string | number>
-        loadData={reFetchArticle}
-        renderData={renderArticle}
-      ></LoadingWrapper>
-    </>
-  );
-}
