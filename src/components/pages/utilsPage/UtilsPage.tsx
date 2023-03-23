cd9f0dd5ce126c172414a12374f74a969c387e15
Patrick <patricktsaikl@gmail.com>
2023-03-23 23:43:06 +0800

    custom tab
    practice custom tab

--- src/components/pages/utilsPage/UtilsPage.tsx
+++ src/components/pages/utilsPage/UtilsPage.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import ShowArticle from '../../common/loading/ShowArticle';
 import FormComponent from '../../../hooks/useForm/FormComponent';
+import CustomTabs, { TabProps, CustomTab } from '../../common/tabs/CustomTabs';
 
 const UtilsPage = () => {
+  const tabArrays: ReactElement<TabProps>[] = [
+    <CustomTab key="showArticle" eventKey="showArticle">
+      <ShowArticle />
+    </CustomTab>,
+    <CustomTab key="formComponent" eventKey="formComponent">
+      <FormComponent />
+    </CustomTab>,
+  ];
   return (
     <div>
       Utils page
-      <ShowArticle />
-      <FormComponent />
+      <div
+        className="tabsComponet"
+        style={{
+          width: '80%',
+          margin: 'auto',
+          border: '1px solid gray',
+          padding: '1rem',
+        }}
+      >
+        <CustomTabs tabsArray={tabArrays} activeKey="formComponent" />
+      </div>
     </div>
   );
 };
